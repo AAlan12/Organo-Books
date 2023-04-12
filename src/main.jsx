584e80e79a788153f0109986794e9e70e90ca9eb
Antonio Alan <tonio.alan07@gmail.com>
2023-04-12 06:16:31 -0300

    Limpado e criando pastas iniciais

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './styles/generic/reset.css'
 import App from './App'
 import './index.css'
+import './styles/setting/colors.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
