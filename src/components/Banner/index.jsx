0cdde7f53cf7f9434829e3daa721b83586705528
Antonio Alan <tonio.alan07@gmail.com>
2023-04-24 13:55:50 -0300

    Estilização do component Banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,14 +1,24 @@
 import styles from './styles.module.css';
 import ImgBanner from '../../assets/img/books.jpg'
+import Logo from '../../assets/img/logo.png'
 
 
 function Banner() {
-  return (
-    <div>
-        <img src={ImgBanner} alt="Livros" />
-        <p>Organo Books</p>
-    </div>
-  );
+    return (
+        <div className={styles.container_banner}>
+            <div className={styles.container_logo}>
+                <img src={Logo} alt="Logo da Organo Books" />
+                <h2>Organo Books</h2>
+            </div>
+            <div className={styles.container_box}>
+                <div className={styles.container_subtitle}>
+                    <h3>Seus livros, quadrinhos e mangás</h3>
+                    <p>organizados em um só lugar!</p>
+                </div>
+                <img src={ImgBanner} alt="Livros" />
+            </div>
+        </div>
+    );
 }
 
 export default Banner;
